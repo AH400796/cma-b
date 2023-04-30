656d786d317a45d60ac32f60bcf5bb3fe8a9dd69
AH400796 <A.Hokhman@gmail.com>
2023-04-30 19:16:56 +0300

    1

--- src/data/fee.js
+++ src/data/fee.js
@@ -26,140 +26,158 @@ const client = Binance({
 // GET fee
 const getWithdrawFeesList = async function () {
   const feesData = {};
-  try {
-    await getExmoFee(feesData);
-    await getBinanceFee(feesData);
-    await getOkxFee(feesData);
-    await getMexcFee(feesData);
-    // await getBybitFee(feesData);
-  } catch (error) {
-    console.log(error.message);
-  }
+  getExmoFee(feesData);
+  getBinanceFee(feesData);
+  getOkxFee(feesData);
+  getMexcFee(feesData);
+  // getBybitFee(feesData);
+
   fs.writeFile(feesPath, JSON.stringify(feesData), "utf8");
 };
 
 // MEXC
 async function getMexcFee(feesData) {
-  const result = await axios.get("https://www.mexc.com/open/api/v2/market/coin/list");
-  feesData.mexc = [];
+  try {
+    const result = await axios.get("https://www.mexc.com/open/api/v2/market/coin/list");
+    feesData.mexc = [];
 
-  result.data.data.forEach(el => {
-    const symbol = el.currency;
-    el.coins.forEach(el => {
-      const amount = el.fee;
-      const coin = symbol;
-      const network = el.chain;
-      const minWithValue = el.withdraw_limit_min;
-      const fee = makeFee(amount, coin, network, minWithValue, symbol);
-      feesData.mexc.push(fee);
+    result.data.data.forEach(el => {
+      const symbol = el.currency;
+      el.coins.forEach(el => {
+        const amount = el.fee;
+        const coin = symbol;
+        const network = el.chain;
+        const minWithValue = el.withdraw_limit_min;
+        const fee = makeFee(amount, coin, network, minWithValue, symbol);
+        feesData.mexc.push(fee);
+      });
     });
-  });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 // OKX
 async function getOkxFee(feesData) {
-  feesData.okx = [];
-  const timestamp = new Date(Date.now()).toISOString();
-  const string = timestamp + "GET" + `/api/v5/asset/currencies` + "";
-  const sign = crypto.createHmac("sha256", OKXAPISecret).update(string).digest("base64");
-  const config = {
-    method: "GET",
-    url: `https://www.okx.com/api/v5/asset/currencies`,
-    headers: {
-      "OK-ACCESS-KEY": OKXAPIKey,
-      "OK-ACCESS-SIGN": sign,
-      "OK-ACCESS-TIMESTAMP": timestamp,
-      "OK-ACCESS-PASSPHRASE": OKXPassphrase,
-    },
-  };
-  const result = await axios(config);
-  result.data.data.forEach(el => {
-    const amount = Number(el.minFee);
-    const coin = el.ccy;
-    const network = el.chain.includes("ERC20") ? "ERC20" : el.chain;
-    const minWithValue = Number(el.minWd);
-    const symbol = el.ccy;
-    const fee = makeFee(amount, coin, network, minWithValue, symbol);
-    feesData.okx.push(fee);
-  });
+  try {
+    feesData.okx = [];
+    const timestamp = new Date(Date.now()).toISOString();
+    const string = timestamp + "GET" + `/api/v5/asset/currencies` + "";
+    const sign = crypto.createHmac("sha256", OKXAPISecret).update(string).digest("base64");
+    const config = {
+      method: "GET",
+      url: `https://www.okx.com/api/v5/asset/currencies`,
+      headers: {
+        "OK-ACCESS-KEY": OKXAPIKey,
+        "OK-ACCESS-SIGN": sign,
+        "OK-ACCESS-TIMESTAMP": timestamp,
+        "OK-ACCESS-PASSPHRASE": OKXPassphrase,
+      },
+    };
+    const result = await axios(config);
+    result.data.data.forEach(el => {
+      const amount = Number(el.minFee);
+      const coin = el.ccy;
+      const network = el.chain.includes("ERC20") ? "ERC20" : el.chain;
+      const minWithValue = Number(el.minWd);
+      const symbol = el.ccy;
+      const fee = makeFee(amount, coin, network, minWithValue, symbol);
+      feesData.okx.push(fee);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 // Bybit
 // async function getBybitFee(feesData) {
-//   feesData.bybit = [];
-//   const timestamp = Date.now().toString();
-//   const sign = crypto
-//     .createHmac("sha256", ByBitAPISecret)
-//     .update(timestamp + ByBitAPIKey + "50000")
-//     .digest("hex");
+//   try {
+//     feesData.bybit = [];
+//     const timestamp = Date.now().toString();
+//     const sign = crypto
+//       .createHmac("sha256", ByBitAPISecret)
+//       .update(timestamp + ByBitAPIKey + "50000")
+//       .digest("hex");
 
-//   const config = {
-//     method: "GET",
-//     url: "https://api.bybit.com/v5/asset/coin/query-info",
-//     headers: {
-//       "X-BAPI-SIGN-TYPE": "2",
-//       "X-BAPI-SIGN": sign,
-//       "X-BAPI-API-KEY": ByBitAPIKey,
-//       "X-BAPI-TIMESTAMP": timestamp,
-//       "X-BAPI-RECV-WINDOW": "50000",
-//       "Content-Type": "application/json; charset=utf-8",
-//     },
-//   };
-//   const result = await axios(config);
-//   console.log("data", result.data.result.rows);
-//   result?.data?.result.rows.forEach(el => {
-//     const coinName = el.name;
-//     const symbolName = el.coin;
-//     el.chains.forEach(el => {
-//       const amount = Number(el.withdrawFee);
-//       const coin = coinName;
-//       const network = el.chainType;
-//       const minWithValue = Number(el.withdrawMin);
-//       const symbol = symbolName;
-//       const fee = makeFee(amount, coin, network, minWithValue, symbol);
+//     const config = {
+//       method: "GET",
+//       url: "https://api.bybit.com/v5/asset/coin/query-info",
+//       headers: {
+//         "X-BAPI-SIGN-TYPE": "2",
+//         "X-BAPI-SIGN": sign,
+//         "X-BAPI-API-KEY": ByBitAPIKey,
+//         "X-BAPI-TIMESTAMP": timestamp,
+//         "X-BAPI-RECV-WINDOW": "50000",
+//         "Content-Type": "application/json; charset=utf-8",
+//       },
+//     };
+//     const result = await axios(config);
+//     console.log("data", result.data.result.rows);
+//     result?.data?.result.rows.forEach(el => {
+//       const coinName = el.name;
+//       const symbolName = el.coin;
+//       el.chains.forEach(el => {
+//         const amount = Number(el.withdrawFee);
+//         const coin = coinName;
+//         const network = el.chainType;
+//         const minWithValue = Number(el.withdrawMin);
+//         const symbol = symbolName;
+//         const fee = makeFee(amount, coin, network, minWithValue, symbol);
 
-//       feesData.bybit.push(fee);
+//         feesData.bybit.push(fee);
+//       });
 //     });
-//   });
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+
 // }
 
 // Binance
 async function getBinanceFee(feesData) {
-  feesData.binance = [];
-  const result = await client.capitalConfigs();
-  result.forEach(el =>
-    el.networkList.forEach(el => {
-      if (Number(el.withdrawMin) === 0) {
-        return;
-      }
-      const amount = Number(el.withdrawFee);
-      const coin = el.coin;
-      const network = el.name;
-      const minWithValue = Number(el.withdrawMin);
-      const symbol = el.coin;
-      const fee = makeFee(amount, coin, network, minWithValue, symbol);
-      feesData.binance.push(fee);
-    })
-  );
+  try {
+    feesData.binance = [];
+    const result = await client.capitalConfigs();
+    result.forEach(el =>
+      el.networkList.forEach(el => {
+        if (Number(el.withdrawMin) === 0) {
+          return;
+        }
+        const amount = Number(el.withdrawFee);
+        const coin = el.coin;
+        const network = el.name;
+        const minWithValue = Number(el.withdrawMin);
+        const symbol = el.coin;
+        const fee = makeFee(amount, coin, network, minWithValue, symbol);
+        feesData.binance.push(fee);
+      })
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 // EXMO
 async function getExmoFee(feesData) {
-  const result = await axios.get("https://api.exmo.com/v1.1/payments/providers/crypto/list");
-  feesData.exmo = [];
-  Object.keys(result.data).forEach(el =>
-    result.data[el].forEach(el => {
-      if (el.commission_desc.includes("%") || el.commission_desc === "") {
-        return;
-      }
-      const amount = Number(el.commission_desc.replace(/[^\d.]/g, ""));
-      const coin = el.commission_desc.replace(/[^a-zA-Z]/g, "");
-      const network = el.comment.includes("ERC20") ? `${coin}(ERC20)` : `${coin}`;
-      const minWithValue = parseFloat(el.min);
-      const symbol = el.currency_name;
-      const fee = makeFee(amount, coin, network, minWithValue, symbol);
-      feesData.exmo.push(fee);
-    })
-  );
+  try {
+    const result = await axios.get("https://api.exmo.com/v1.1/payments/providers/crypto/list");
+    feesData.exmo = [];
+    Object.keys(result.data).forEach(el =>
+      result.data[el].forEach(el => {
+        if (el.commission_desc.includes("%") || el.commission_desc === "") {
+          return;
+        }
+        const amount = Number(el.commission_desc.replace(/[^\d.]/g, ""));
+        const coin = el.commission_desc.replace(/[^a-zA-Z]/g, "");
+        const network = el.comment.includes("ERC20") ? `${coin}(ERC20)` : `${coin}`;
+        const minWithValue = parseFloat(el.min);
+        const symbol = el.currency_name;
+        const fee = makeFee(amount, coin, network, minWithValue, symbol);
+        feesData.exmo.push(fee);
+      })
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 function makeFee(amount, coin, network, minWithValue, symbol) {
--- src/services/ByBit.js
+++ src/services/ByBit.js
@@ -1,77 +1,77 @@
-const axios = require("axios");
-const path = require("path");
-const fs = require("fs").promises;
-const feesPath = path.join(__dirname, "../data/fees.json");
-// const WebSocket = require("ws");
-// const crypto = require("crypto");
-// const { ByBitAPIKey, ByBitAPISecret } = process.env;
-// const apiKey = ByBitAPIKey;
-// const recvWindow = 59000;
-// const timestamp = Date.now().toString();
+// const axios = require("axios");
+// const path = require("path");
+// const fs = require("fs").promises;
+// const feesPath = path.join(__dirname, "../data/fees.json");
+// // const WebSocket = require("ws");
+// // const crypto = require("crypto");
+// // const { ByBitAPIKey, ByBitAPISecret } = process.env;
+// // const apiKey = ByBitAPIKey;
+// // const recvWindow = 59000;
+// // const timestamp = Date.now().toString();
 
-const getPairOrders = async function () {
-  const result = await axios.get("https://api.bybit.com/spot/v3/public/quote/ticker/bookTicker");
-  return result.data.result.list;
-};
-// Data processing
-async function bybitData(data, exclusions) {
-  const result = await fs.readFile(feesPath, "utf8");
-  const fees = JSON.parse(result).bybit || [];
-  const regEx = /USDT/;
-  const bybitOrders = await getPairOrders();
-  const bybitUSDTOrders = bybitOrders.filter(el => regEx.test(el.symbol) & (Number(el.bidPrice) !== 0) & !el.symbol.startsWith("USDT"));
+// const getPairOrders = async function () {
+//   const result = await axios.get("https://api.bybit.com/spot/v3/public/quote/ticker/bookTicker");
+//   return result.data.result.list;
+// };
+// // Data processing
+// async function bybitData(data, exclusions) {
+//   const result = await fs.readFile(feesPath, "utf8");
+//   const fees = JSON.parse(result).bybit || [];
+//   const regEx = /USDT/;
+//   const bybitOrders = await getPairOrders();
+//   const bybitUSDTOrders = bybitOrders.filter(el => regEx.test(el.symbol) & (Number(el.bidPrice) !== 0) & !el.symbol.startsWith("USDT"));
 
-  bybitUSDTOrders.map(el => {
-    const pair = el.symbol.replace(/USDT/g, "/USDT");
-    if (exclusions.find(el => el.market === "BYBIT" && el.pair === pair)) {
-      return null;
-    }
+//   bybitUSDTOrders.map(el => {
+//     const pair = el.symbol.replace(/USDT/g, "/USDT");
+//     if (exclusions.find(el => el.market === "BYBIT" && el.pair === pair)) {
+//       return null;
+//     }
 
-    const symbol = el.symbol.replace(/USDT/g, "");
-    const bybitFee = fees.filter(el => el.symbol === symbol).map(el => el.fee);
-    const { askPrice, askQty, bidPrice, bidQty } = el;
-    const precision = Number(askPrice) < 0.01 ? 0 : Number(Number(askPrice).toFixed(1).toString().indexOf(".")) + 1;
-    const pairData = {
-      market: "BYBIT",
-      url: `https://www.bybit.com/uk-UA/trade/spot/${pair}`,
-      buyPrice: Number(askPrice),
-      buyQty: Number(Number(askQty).toFixed(precision)),
-      sellPrice: Number(bidPrice),
-      sellQty: Number(Number(bidQty).toFixed(precision)),
-      fee: bybitFee,
-      withdrlUrl: `https://www.bybit.com/user/assets/home/spot`,
-      depUrl: `https://www.bybit.com/user/assets/home/spot`,
-    };
-    if (!data[pair]) {
-      data[pair] = [];
-    }
-    data[pair].push(pairData);
-    return null;
-  });
+//     const symbol = el.symbol.replace(/USDT/g, "");
+//     const bybitFee = fees.filter(el => el.symbol === symbol).map(el => el.fee);
+//     const { askPrice, askQty, bidPrice, bidQty } = el;
+//     const precision = Number(askPrice) < 0.01 ? 0 : Number(Number(askPrice).toFixed(1).toString().indexOf(".")) + 1;
+//     const pairData = {
+//       market: "BYBIT",
+//       url: `https://www.bybit.com/uk-UA/trade/spot/${pair}`,
+//       buyPrice: Number(askPrice),
+//       buyQty: Number(Number(askQty).toFixed(precision)),
+//       sellPrice: Number(bidPrice),
+//       sellQty: Number(Number(bidQty).toFixed(precision)),
+//       fee: bybitFee,
+//       withdrlUrl: `https://www.bybit.com/user/assets/home/spot`,
+//       depUrl: `https://www.bybit.com/user/assets/home/spot`,
+//     };
+//     if (!data[pair]) {
+//       data[pair] = [];
+//     }
+//     data[pair].push(pairData);
+//     return null;
+//   });
 
-  // const client = new WebSocket("wss://stream.bybit.com/spot/public/v3");
-  //   client.on("open", function () {
-  //     setInterval(() => {
-  //       client.ping();
-  //     }, 20000);
-  //     client.ping();
-  //     client.send(JSON.stringify({ op: "subscribe", args: [`orderbook.1.${el.symbol}`] }));
-  //   });
-  //   client.on("message", function (data) {
-  //   console.log(Buffer.from(data).toString());
-  //   console.log("===/", JSON.parse(Buffer.from(data).toString()));
-  // const pair = "ETHUSDT";
-  // const client = new WebSocket("wss://stream.bybit.com/spot/public/v3");
-  // client.on("open", function () {
-  //   setInterval(() => {
-  //     client.ping();
-  //   }, 20000);
-  //   client.ping();
-  //   client.send(JSON.stringify({ op: "subscribe", args: [`orderbook.5.${pair}`] }));
-  // });
-  // client.on("message", function (data) {
-  //   console.log("===/", JSON.parse(Buffer.from(data).toString()).data.s);
-  // });
-}
+//   // const client = new WebSocket("wss://stream.bybit.com/spot/public/v3");
+//   //   client.on("open", function () {
+//   //     setInterval(() => {
+//   //       client.ping();
+//   //     }, 20000);
+//   //     client.ping();
+//   //     client.send(JSON.stringify({ op: "subscribe", args: [`orderbook.1.${el.symbol}`] }));
+//   //   });
+//   //   client.on("message", function (data) {
+//   //   console.log(Buffer.from(data).toString());
+//   //   console.log("===/", JSON.parse(Buffer.from(data).toString()));
+//   // const pair = "ETHUSDT";
+//   // const client = new WebSocket("wss://stream.bybit.com/spot/public/v3");
+//   // client.on("open", function () {
+//   //   setInterval(() => {
+//   //     client.ping();
+//   //   }, 20000);
+//   //   client.ping();
+//   //   client.send(JSON.stringify({ op: "subscribe", args: [`orderbook.5.${pair}`] }));
+//   // });
+//   // client.on("message", function (data) {
+//   //   console.log("===/", JSON.parse(Buffer.from(data).toString()).data.s);
+//   // });
+// }
 
-module.exports = bybitData;
+// module.exports = bybitData;
